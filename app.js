38bd77a7f0c7e98983c90530bff1bc11ead378d8
Manoj <manoj.saini@metacube.com>
2018-04-14 13:02:24 +0530

    Added field in forms

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var app = angular.module('app', ['ngRoute','ngResource'])
 .run(function($rootScope) {
     $rootScope.customerData = [
-	        {name:"Name1" , lastName:"Name2" , email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan" },
-			  {name:"Name1" , lastName:"Name2" , email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan" },
-			  {name:"Name1" , lastName:"Name2" , email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan" },
-			  {name:"Name1" , lastName:"Name2" , email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan" },
-			  {name:"Name1" , lastName:"Name2" , email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan" },
-			  {name:"Name1" , lastName:"Name2" , email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan" },
-			  {name:"Name1" , lastName:"Name2" , email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan" },
-			  {name:"Name1" , lastName:"Name2" , email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan" },
-			  {name:"Name1" , lastName:"Name2" , email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan" },
-	   
+	        {name:"Name1" , lastName:"Name2" , contactPerson:"Person1",gstn:"1452",additionalPhon:"456",email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan",pincode:"301001" },
+			  {name:"Name1" , lastName:"Name2" , contactPerson:"Person1",gstn:"1452",additionalPhon:"456",email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan",pincode:"301001" },
+			  {name:"Name1" , lastName:"Name2" , contactPerson:"Person1",gstn:"1452",additionalPhon:"456",email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan",pincode:"301001" },
+			  {name:"Name1" , lastName:"Name2" , contactPerson:"Person1",gstn:"1452",additionalPhon:"456",email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan",pincode:"301001" },
+			  {name:"Name1" , lastName:"Name2" , contactPerson:"Person1",gstn:"1452",additionalPhon:"456",email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan",pincode:"301001" },
+			  {name:"Name1" , lastName:"Name2" , contactPerson:"Person1",gstn:"1452",additionalPhon:"456",email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan",pincode:"301001" },
+			  {name:"Name1" , lastName:"Name2" , contactPerson:"Person1",gstn:"1452",additionalPhon:"456",email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan",pincode:"301001" },
+			  {name:"Name1" , lastName:"Name2" , contactPerson:"Person1",gstn:"1452",additionalPhon:"456",email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan",pincode:"301001" },
+			  {name:"Name1" , lastName:"Name2" , contactPerson:"Person1",gstn:"1452",additionalPhon:"456",email: "email" , phon:"12345" , fixedLine:"457878" , address:"Address 1" , city:"Jaipur", state:"Rajasthan",pincode:"301001" },
 	  ];
 });
 
